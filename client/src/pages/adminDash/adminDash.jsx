badb1fcf5224f5ce4686957b156f440edc1c9a9d
Naveen-works <naveenkarthik26042005@gmail.com>
2025-03-23 07:57:15 +0530

    Creation of pages

---
+++ client/src/pages/adminDash/adminDash.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './adminDash.css'
+const adminDash = () => {
+    return (
+        <div>
+            Admin Dashboard
+        </div>
+    )
+}
+
+export default adminDash
